dd3bc213da17115b93ae86a813b759aa33593f75
pablowbk <pablo.weisbek@gmail.com>
2021-01-16 22:42:48 -0300

    fix background full height

--- src/Login.js
+++ src/Login.js
@@ -2,12 +2,14 @@ import React, {useState, useEffect} from "react";
 
 function Login() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
   
   useEffect(()=> {
     const fetchUsers = async () => {
         const res = await fetch(process.env.REACT_APP_API_URL + '/users');
         const jsonRes = await res.json();
         setUsers(jsonRes);
+        setLoading(false);
       }
       fetchUsers()
     }, [])
@@ -17,9 +19,8 @@ function Login() {
       <h1>Logged in!</h1>
       <section>
         <h3>List of active users:</h3>
-        <ul>
-          {users.map(user => <li key={user._id}><p><span>{user.username}</span> =&gt; <span>{user.email}</span></p></li> )}
-        </ul>
+        {loading ? <p>loading...</p> : <ul>{users.map(user => <li key={user._id}><p><span>{user.username}</span> =&gt; <span>{user.email}</span></p></li> )}</ul>}
+        
       </section>
     </div>
   )
